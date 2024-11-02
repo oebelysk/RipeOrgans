40f6099c1c01b40c0563eaf7fc8d2ab4f9909482
Oebelysk <oebelysk@proton.me>
2024-11-02 18:07:23 -0400

    Initialization 2

---
+++ Entites/Items/Cleaver/cleaver_physical.gd
@@ -0,0 +1,15 @@
+extends "res://RipeOrgans/Entites/Items/physical_item.gd"
+
+@onready var cleaver_item = $RigidBody3D
+
+var item_type = "WEAPON"
+var equipped = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,79 +1,91 @@
 extends CharacterBody3D
 
-@onready var Camera = $Camera
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 7.5
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.0025
 
-# Camera variables
-var camera_rotation = Vector2(0, 0)
-@export var mouse_sensitivity = 0.001
-
-# Movement variables
-@export var walk_speed: float = 3
-@export var jump_velocity: float = 4.5
-@export var fall_acceleration: float = 75
-@export var player_deceleration: float = 0.15
-@export var player_acceleration: float = 0.3
+# Bob variables
+const BOB_FREQ = 2
+const BOB_AMP = 0.04
+var t_bob = 0.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 9.8
+
+@onready var head = $Head
+@onready var camera = $Head/Camera
+@onready var body = $CollisionShape3D
+@onready var animation_player = $AnimationPlayer
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
+func _input(event):
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
+		
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		body.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-85), deg_to_rad(85))
+		
+	if event.is_action_pressed("primary_attack"):
+		animation_player.play("attack_topleft_to_bottomright")
 
-func _ragdoll(force: Vector3):
+func _unhandled_input(event):
 	pass
 
-func _input(event):
-	if event.is_action_pressed("ui_cancel"):
-		get_tree().quit()
-	
-	if event is InputEventMouseMotion:
-		var mouse_event = event.relative * mouse_sensitivity
-		camera_look(mouse_event)
-		
-func camera_look(movement: Vector2):
-	camera_rotation += movement
-	camera_rotation.y = clamp(camera_rotation.y, -1.5, 1.2)
-	
-	transform.basis = Basis()
-	Camera.transform.basis = Basis()
-	
-	# rotate y
-	rotate_object_local(Vector3(0,1,0), -camera_rotation.x)
-	# rotate x
-	Camera.rotate_object_local(Vector3(1,0,0), -camera_rotation.y)
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	
-	if direction:
-		var speed: float = 0
-		
-		speed = walk_speed
-		
-		# Accelerate towards move direction, but decelerate velocities not towards move direction
-		velocity.x = move_toward(velocity.x, direction.x * speed, abs(velocity.x * player_deceleration) + abs(direction.x * player_acceleration))
-		velocity.z = move_toward(velocity.z, direction.z * speed, abs(velocity.z * player_deceleration) + abs(direction.z * player_acceleration)) 
-		print(velocity.x)
-		print(velocity.z)
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
 	else:
-		# Decelerate towards 0 velocity at a rate proportional to velocity
-		velocity.x = move_toward(velocity.x, 0, abs(velocity.x * player_deceleration))
-		velocity.z = move_toward(velocity.z, 0, abs(velocity.z * player_deceleration))
+		speed = WALK_SPEED
 		
-# Stop deceleration calculations when velocity is negligible
-	if abs(velocity.x) < 0.05:
-		velocity.x = 0
-	if abs(velocity.z) < 0.05:
-		velocity.z = 0
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if is_on_floor():
+		if direction:
+			velocity.x = move_toward(velocity.x, direction.x * speed, 1)
+			velocity.z = move_toward(velocity.z, direction.z * speed, 1)
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7)
+			if abs(velocity.x) < 0.005:
+				velocity.x = 0
+			if abs(velocity.z) < 0.005:
+				velocity.z = 0
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 5)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 5)
+
+		
+	# Head bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
 	move_and_slide()
+	
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	return pos
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "attack_topleft_to_bottomright":
+		animation_player.play("idle")
